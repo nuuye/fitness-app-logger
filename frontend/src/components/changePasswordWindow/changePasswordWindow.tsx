936aee87469895ef72481f4cebd5710671dc24c7
Thomas Mostowfi <thomassmostowfi@gmail.com>
2025-04-14 11:45:39 +0200

    logic for password length

--- frontend/src/components/changePasswordWindow/changePasswordWindow.tsx
+++ frontend/src/components/changePasswordWindow/changePasswordWindow.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./changePassword.module.scss";
 import { Button, Card, Input } from "@mui/material";
+import { useUser } from "../../context/userContext";
 
 interface changePasswordWindowProps {
     onCancel?: () => void;
@@ -8,19 +9,20 @@ interface changePasswordWindowProps {
 }
 
 export default function ChangePasswordWindow({ onCancel, onConfirm }: changePasswordWindowProps) {
+    const { user, userInitials, setUser } = useUser();
     const [isExiting, setIsExiting] = useState<boolean>(false);
-    const [currentPassword, setCurrendPassword] = useState<string>("");
+    const [currentPassword, setCurrentPassword] = useState<string>("");
     const [newPassword, setNewPassword] = useState<string>("");
     const [confirmPassword, setConfirmPassword] = useState<string>("");
     const [canSave, setCanSave] = useState<boolean>(false);
     const [displayPasswordMatch, setDisplayPasswordMatch] = useState<boolean>(false);
+    const [displayPasswordLengthWarning, setDisplayPasswordLengthWarning] = useState<boolean>(false);
 
     const handleClose = (callback: () => void, resetFields?: boolean) => {
         if (resetFields) {
-            setCurrendPassword("");
+            setCurrentPassword("");
             setNewPassword("");
             setConfirmPassword("");
-            console.log(currentPassword, newPassword, confirmPassword);
         }
         setIsExiting(true);
         setTimeout(() => {
@@ -29,40 +31,51 @@ export default function ChangePasswordWindow({ onCancel, onConfirm }: changePass
     };
 
     useEffect(() => {
-        const allFieldsFilled = currentPassword.length > 0 && newPassword.length > 0 && confirmPassword.length > 0;
+        const allFieldsFilled = newPassword.length > 0 && confirmPassword.length > 0;
         const passwordsMatch = newPassword === confirmPassword;
+        setDisplayPasswordLengthWarning(newPassword && newPassword.length < 6);
         setDisplayPasswordMatch(allFieldsFilled && !passwordsMatch);
-        setCanSave(allFieldsFilled && passwordsMatch);
+        setCanSave(currentPassword.length > 0 && allFieldsFilled && passwordsMatch && newPassword.length >= 6);
     }, [currentPassword, newPassword, confirmPassword]);
 
     return (
         <div className={`${styles.rootContainer} ${isExiting ? styles.exit : ""}`}>
             <Card variant="outlined" className={styles.card}>
                 <div className={styles.cardBody}>
-                    <span>Current password:</span>
-                    <Input
-                        className={styles.inputs}
-                        type="password"
-                        value={currentPassword}
-                        onChange={(e) => setCurrendPassword(e.target.value)}
-                    />
-                    <span>New password:</span>
-                    <Input
-                        className={styles.inputs}
-                        type="password"
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)}
-                    />
-                    <span>Confirm new password:</span>
-                    <Input
-                        className={styles.inputs}
-                        type="password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                    {displayPasswordMatch && (
-                        <span className={`${styles.passwordMatch} ${styles.show}`}>Passwords must match!</span>
-                    )}
+                    <form autoComplete="off">
+                        <span>Current password:</span>
+                        <Input
+                            autoComplete="new-password"
+                            className={styles.inputs}
+                            type="password"
+                            value={currentPassword}
+                            onChange={(e) => setCurrentPassword(e.target.value)}
+                        />
+                        <span>New password:</span>
+                        <Input
+                            autoComplete="new-password"
+                            className={styles.inputs}
+                            type="password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
+                        {displayPasswordLengthWarning && (
+                            <span className={`${styles.passwordLengthWarning} ${styles.show}`}>
+                                Password must be at least 6 characters long.
+                            </span>
+                        )}
+                        <span>Confirm new password:</span>
+                        <Input
+                            autoComplete="new-password"
+                            className={styles.inputs}
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                        {displayPasswordMatch && (
+                            <span className={`${styles.passwordMatch} ${styles.show}`}>Passwords must match!</span>
+                        )}
+                    </form>
                 </div>
                 <div className={styles.actions}>
                     <Button variant="outlined" onClick={() => handleClose(onCancel, true)}>
