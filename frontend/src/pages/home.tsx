19475eec6104b7cb7ba7a003719f19e92abc9916
Thomas Mostowfi <thomassmostowfi@gmail.com>
2025-05-18 17:14:18 +0200

    home page structure

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,68 @@
+import styles from "./home.module.scss";
+import SideBar, { SideBarRef } from "../components/sideBar/sideBar";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+import AuthWrapper from "../components/authWrapper/authWrapper";
+import DeleteCategoryWindow from "../components/deleteCategoryWindow/deleteCategoryWindow";
+
+export default function Home() {
+    const router = useRouter();
+    const sideBarRef = useRef<SideBarRef>(null);
+
+    const [sideBarOpen, setSideBarOpen] = useState<boolean>(false);
+    const [showDeleteCategoryWindow, setShowDeleteCategoryWindow] = useState<boolean>(false);
+    const [tempCategory, setTempCategory] = useState<{ id: string; label: string }>(null);
+    const [hideMenu, setHideMenu] = useState<boolean>(true);
+
+    useEffect(() => {
+        setSideBarOpen(localStorage.getItem("sideBarOpen") === "true");
+    }, []);
+
+    const handleCancelWindow = () => {
+        setShowDeleteCategoryWindow(!showDeleteCategoryWindow);
+    };
+
+    //handles the display of sub categories on dashboard whenever click
+    const handleSubCategory = async (categoryId: string) => {
+        localStorage.setItem("subCategoryId", categoryId);
+        router.push("/dashboard");
+    };
+
+    const triggerDeleteCategory = (id: string) => {
+        console.log("triggered");
+        sideBarRef.current?.handleCategoryDelete(id);
+    };
+
+    return (
+        <AuthWrapper>
+            <div className={styles.root}>
+                {showDeleteCategoryWindow && (
+                    <DeleteCategoryWindow
+                        isCategory
+                        label={tempCategory.label}
+                        onCancel={handleCancelWindow}
+                        onConfirm={() => {
+                            triggerDeleteCategory(tempCategory.id);
+                            handleCancelWindow();
+                        }}
+                    />
+                )}
+                <SideBar
+                    retrieveSubCategory={handleSubCategory}
+                    retrieveSideBarStatus={setSideBarOpen}
+                    onClickDelete={(categoryId, label) => {
+                        handleCancelWindow();
+                        setTempCategory({ id: categoryId, label: label });
+                    }}
+                    ref={sideBarRef}
+                    retrieveShowMenuStatus={setHideMenu}
+                />
+                <div
+                    className={`${styles.mainContainer} ${!sideBarOpen && styles.extendedMainContainer} ${
+                        !hideMenu && styles.isMenuOpen
+                    }`}
+                ></div>
+            </div>
+        </AuthWrapper>
+    );
+}
