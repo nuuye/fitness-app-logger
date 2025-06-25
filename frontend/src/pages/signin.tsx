58c59c9cb82a9a25a4ce5392615e4ca432e937ef
nuye <thomassmostowfi@gmail.com>
2025-06-25 18:06:09 +0200

    added app bar in signIn/Up

--- frontend/src/pages/signin.tsx
+++ frontend/src/pages/signin.tsx
@@ -1,10 +1,16 @@
 import SignIn from "../components/signIn/signIn";
 import styles from "./signin.module.scss";
+import AppBar from '../components/appBar/appBar';
 
 export default function signin() {
     return (
         <div className={styles.root}>
-            <SignIn />
+            <div className={styles.header}>
+                <AppBar />
+            </div>
+            <div className={styles.body}>
+                <SignIn />
+            </div>
         </div>
     );
 }
--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,10 +1,16 @@
+import AppBar from "../components/appBar/appBar";
 import SignUp from "../components/signUp/signUp";
 import styles from "./signup.module.scss";
 
 export default function Identification() {
     return (
         <div className={styles.root}>
-            <SignUp />
+            <div className={styles.header}>
+                <AppBar />
+            </div>
+            <div className={styles.body}>
+                <SignUp />
+            </div>
         </div>
     );
 }
